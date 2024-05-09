128ed42b981ba8a8acd19d79807089895c74ac13
Tulio Lemos <tlcbrasil164@gmail.com>
2024-05-09 16:12:15 -0300

    New Springboot project with MongoDB created!

---
+++ src/main/java/br/com/studiesMongoDB/StudiesMongoDbApplication.java
@@ -0,0 +1,13 @@
+package br.com.studiesMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiesMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudiesMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/studiesMongoDB/StudiesMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.studiesMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudiesMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
