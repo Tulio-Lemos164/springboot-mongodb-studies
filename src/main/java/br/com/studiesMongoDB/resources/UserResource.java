1937c4f0dc8f40d65415eb3c334baff1c0561e1d
Tulio Lemos <tlcbrasil164@gmail.com>
2024-05-09 17:51:58 -0300

    DTO Layer created

--- src/main/java/br/com/studiesMongoDB/resources/UserResource.java
+++ src/main/java/br/com/studiesMongoDB/resources/UserResource.java
@@ -1,6 +1,7 @@
 package br.com.studiesMongoDB.resources;
 
 import br.com.studiesMongoDB.domain.User;
+import br.com.studiesMongoDB.dto.UserDTO;
 import br.com.studiesMongoDB.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -19,8 +20,9 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> myList = userService.findAll();
-        return ResponseEntity.ok().body(myList);
+        List<UserDTO> myListDTO = myList.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(myListDTO);
     }
 }
