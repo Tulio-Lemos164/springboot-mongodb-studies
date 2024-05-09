8a1839298ca98d7dd1b0441f10da2e9c792853ee
Tulio Lemos <tlcbrasil164@gmail.com>
2024-05-09 19:10:55 -0300

    User findById + Exceptions Handling

--- src/main/java/br/com/studiesMongoDB/resources/UserResource.java
+++ src/main/java/br/com/studiesMongoDB/resources/UserResource.java
@@ -6,6 +6,7 @@ import br.com.studiesMongoDB.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,11 @@ public class UserResource {
         List<User> myList = userService.findAll();
         List<UserDTO> myListDTO = myList.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(myListDTO);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 }
