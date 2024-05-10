a57b72407c2470b1f4af49919a4af1f9e95e02dd
Tulio Lemos <tlcbrasil164@gmail.com>
2024-05-09 22:09:46 -0300

    Endpoint to return the posts of a User

--- src/main/java/br/com/studiesMongoDB/resources/UserResource.java
+++ src/main/java/br/com/studiesMongoDB/resources/UserResource.java
@@ -1,5 +1,6 @@
 package br.com.studiesMongoDB.resources;
 
+import br.com.studiesMongoDB.domain.Post;
 import br.com.studiesMongoDB.domain.User;
 import br.com.studiesMongoDB.dto.UserDTO;
 import br.com.studiesMongoDB.services.UserService;
@@ -52,5 +53,11 @@ public class UserResource {
         user.setId(id);
         user = userService.update(user);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user.getPosts());
     }
 }
